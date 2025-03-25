e887ceba6253b1a080bc71dfb70c0ae0a9630d89
Yuldashev <ikrom921@gmail.com>
2025-03-25 14:34:09 +0500

    [Add] TableView

---
+++ Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+import UIKit
+
+final class AccountSummaryViewController: UIViewController {
+  let games = [ "Pacman", "Space Invaders", "Asteroids"]
+  var tableView = UITableView()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setup()
+  }
+}
+
+extension AccountSummaryViewController {
+  func setup() {
+    setupTableView()
+  }
+  
+  private func setupTableView() {
+    tableView.delegate = self
+    tableView.dataSource = self
+    
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(tableView)
+    
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+      tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+      
+    ])
+  }
+}
+
+extension AccountSummaryViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = games[indexPath.row]
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return games.count
+  }
+}
+
+extension AccountSummaryViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+  }
+}
