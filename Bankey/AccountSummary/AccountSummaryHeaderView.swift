427ab3a821dff06f489ee989d526eec80457babb
Yuldashev <ikrom921@gmail.com>
2025-03-25 17:31:32 +0500

    [Add] TableViewHeader

---
+++ Bankey/AccountSummary/AccountSummaryHeaderView.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+final class AccountSummaryHeaderView: UIView {
+  
+  @IBOutlet var contentView: UIView!
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    commonInit()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    commonInit()
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+  }
+  
+  private func commonInit() {
+    let bundle = Bundle(for: AccountSummaryHeaderView.self)
+    bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+    addSubview(contentView)
+    contentView.backgroundColor = appColor
+    contentView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      contentView.topAnchor.constraint(equalTo: self.topAnchor),
+      contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+      contentView.leftAnchor.constraint(equalTo: self.leftAnchor),
+      contentView.rightAnchor.constraint(equalTo: self.rightAnchor)
+    ])
+  }
+}
--- Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -15,6 +15,7 @@ final class AccountSummaryViewController: UIViewController {
 extension AccountSummaryViewController {
   func setup() {
     setupTableView()
+    setupTableHeaderView()
   }
   
   private func setupTableView() {
@@ -31,6 +32,16 @@ extension AccountSummaryViewController {
       tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
       
     ])
+  }
+  
+  private func setupTableHeaderView() {
+    let header = AccountSummaryHeaderView(frame: .zero)
+    
+    var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+    size.width = UIScreen.main.bounds.width
+    header.frame.size = size
+    
+    tableView.tableHeaderView = header
   }
 }
 
